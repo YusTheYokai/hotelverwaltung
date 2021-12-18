94440fb32c7738275f3536d9b65891b6ad88091f
Nico Lerchl <nico@lerchl.one>
2021-12-18 16:42:00 +0100

    4: Anmerkungen umgesetzt

--- index/createNewsPost.php
+++ index/createNewsPost.php
@@ -7,7 +7,6 @@
     $addQuery = "INSERT INTO news_post (TITLE, CONTENT, PICTURE, USER_ID) VALUES (?, ?, ?, ?);";
     $addStatement = $db->prepare($addQuery);
     $addStatement->bind_param("sssi", $_POST["title"], $_POST["content"], $fileName, $_SESSION["user"]["ID"]);
-    error_log($_POST["title"] . $_POST["content"] . $_POST["picture"]);
     $addStatement->execute();
     header("Location: ../index.php");
 ?>
