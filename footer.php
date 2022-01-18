f68a5e7e42ab2daea6967875fb6ab0f62883ea61
Janko Hu <hu.janko@yahoo.de>
2022-01-18 11:24:37 +0100

    help und imprint ohne bilder

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer class="fixed-bottom footer">
     <div class="container-fluid" style="display: flex; justify-content: space-evenly;">
-        <a class="nav-link" href="/pages/imprint.html">
+        <a class="nav-link" href="/pages/imprint.php">
             Impressum<img src="/icon/info-circle-fill.svg" class="icon" alt="Info-Icon" />
         </a>
-        <a class="nav-link" href="/pages/help.html">
+        <a class="nav-link" href="/pages/help.php">
             Hilfe<img src="/icon/question-circle-fill.svg" class="icon" alt="Question-Icon" />
         </a>
     </div>
