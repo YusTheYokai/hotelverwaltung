f0d0acf162b1ee3204a5b0dd8374076612eea8dd
Janko Hu <hu.janko@yahoo.de>
2022-01-19 18:11:43 +0100

    6: Bilder zu Impressum

--- pages/imprint.php
+++ pages/imprint.php
@@ -5,6 +5,8 @@
 <html lang="de">
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Hotelverwaltung - Impressum</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link href="/css/theme.css" rel="stylesheet">
@@ -13,10 +15,15 @@
         <?php
             include "../menubar.php";
         ?>
+
         <div id="imprintContainer" class="container-fluid overlay quarter-width text-center footer-margin-bottom">
             <h1>Impressum</h1>
             <p>Hotelverwaltung Hu-Lerchl</p>
             <p>Inhaber Hu Janko und Lerchl Nico</p>
+            <picture> 
+              <img style="width: 10vh; height: auto;" src="/images/bild_janko.gif"> 
+              <img style="width: 10vh; height: auto;" src="/images/bild_nico.jpg"> 
+            </picture>  
             <p>Eingetragenes Einzelunternehmen</p>
             <p>Hotel</p>
             <p>UID-Nr: ATU12345678</p>
@@ -34,9 +41,9 @@
                 Sie können allfällige Beschwerde auch an
                 die oben angegebene E-Mail-Adresse
                 richten.</p>
-        </div>        
+        </div>    
         <?php
-            include "../footer.php";
+            require_once "../util/bottomIncludes.php";
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
