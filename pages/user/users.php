b23aae99cc810b10b3b7ad7c512a1925353a0fc3
Nico Lerchl <nico@lerchl.one>
2022-01-19 20:03:15 +0100

    fix users bottom includes

--- pages/user/users.php
+++ pages/user/users.php
@@ -1,12 +1,13 @@
 <?php
-    session_start();
-
-    require_once "../../db/logIntoDB.php";
     require_once "../../guard.php";
     require_once "../../translator.php";
     require_once "../../displayUtils.php";
 
+    session_start();
+
     guardAdmin();
+
+    require_once "../../db/logIntoDB.php";
 
     $columns = "ID,HONORIFIC,LAST_NAME,FIRST_NAME,USERNAME,EMAIL,ROLE,ACTIVE";
     $ignoreColumns = "";
@@ -27,19 +28,17 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Benutzer</title>
+        <title>Hu-Lerchl | Benutzer</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link href="/css/theme.css" rel="stylesheet">
     </head>
     <body>
         <?php
-            include "../../menubar.php";
-
+            require_once "../../menubar.php";
             $tableTitle = "Benutzer";
             $entity = "user.php";
-            include "../../table/table.php";
-            include "../../footer.php";
+            require_once "../../table/table.php";
+            require_once "../../util/bottomIncludes.php";
         ?>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 </html>
